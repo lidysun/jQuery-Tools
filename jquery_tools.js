2d2879f34723f0da8724d9c5c68e8d5bd126ed46
lidysin <820262236@qq.com>
2018-04-12 11:24:28 +0800

    Array.indexOf/remove

--- jquery_tools.js
+++ jquery_tools.js
@@ -22,3 +22,30 @@ $.fn.placeholder = function() {
         });
     }
 };
+
+/**
+ * [indexOf 数组查找某元素]
+ * @param val [要查找的元素]
+ * @return [元素索引位置]
+ */
+Array.prototype.indexOf = function(val) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == val) {
+            return i;
+        }
+    }
+    return -1;
+};
+/**
+ * [remove 从数组中删除指定元素]
+ * @param val [要查找的元素]
+ * @return [修改过后的数组，原数组改变]
+ */
+Array.prototype.remove = function(val) {
+    var index = this.indexOf(val);
+    if (index != -1) {
+        this.splice(index, 1);
+        //重复删除
+        this.remove(val);
+    }
+};
